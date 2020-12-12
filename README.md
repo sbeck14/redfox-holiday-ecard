157ffad4a3870c36c49639d4d1b467159be5e6cb
Sam Beckett <becketts@byu.edu>
2020-12-11 20:07:02 -0700

    fix config, add script files

---
+++ README.md
@@ -0,0 +1,18 @@
+# rfl-emails
+
+## Configuration
+
+`config.json`
+
+This is where your configuration goes.
+
+* user: Gmail username
+* pass: Gmail password
+* from: From email address
+* subject: Subject of email
+
+`emails.txt`
+
+Add your emails to this file, one email per line.
+
+The contents of the email will be read from `email.html`.
---
+++ index.js
@@ -0,0 +1,43 @@
+const asyncPool = require('tiny-async-pool');
+const fs = require('fs');
+const config = require('./config.json');
+
+const htmlEmail = fs.readFileSync('./index.html').toString();
+
+const send = require('gmail-send')({
+  user: config.user,
+  pass: config.pass,
+  from: config.from,
+  subject: config.subject,
+});
+
+const colors = {
+  green: '\u001b[32m',
+  red: '\u001b[31m',
+  reset: '\u001b[0m',
+};
+
+const emails = require('fs').readFileSync('emails.txt', 'utf-8').split('\n');
+
+async function sendEmail(email) {
+  try {
+    await send({
+      'to': email,
+      'html': htmlEmail,
+    });
+    console.log(`${colors.green}✓${colors.reset}  ${email}`);
+  } catch (err) {
+    console.log(`${colors.red}✗${colors.reset}  ${email}\n\t${err}`);
+  }
+}
+
+async function doWork() {
+  try {
+    await asyncPool(2, emails, sendEmail);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+console.log(`Sending ${emails.length} emails...`);
+doWork();
