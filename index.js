fed88626ada5bf44f4213bb4f48d2dfc5f76c822
Sam Beckett <becketts@byu.edu>
2020-12-16 21:56:40 -0700

    add timing, saving success/failed to file

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const asyncPool = require('tiny-async-pool');
 const fs = require('fs');
 const config = require('./config.json');
+const prettyMs = require('pretty-ms');
 
-const htmlEmail = fs.readFileSync('./email.html').toString();
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const success = [];
+const failed = [];
+
+const emailBody = fs.readFileSync('./email.html').toString();
 
 const send = require('gmail-send')({
   user: config.user,
@@ -17,27 +23,45 @@ const colors = {
   reset: '\u001b[0m',
 };
 
-const emails = require('fs').readFileSync('emails.txt', 'utf-8').split('\n');
+const emails = fs.readFileSync('emails.txt', 'utf-8').split('\n');
 
-async function sendEmail(email) {
+async function sendEmail(to) {
+  await snooze(500)
   try {
-    await send({
-      'to': email,
-      'html': htmlEmail,
-    });
-    console.log(`${colors.green}✓${colors.reset}  ${email}`);
+    await send({ to, html: emailBody });
+    success.push(to);
+    console.log(`${colors.green}✓${colors.reset}  ${to}`);
   } catch (err) {
-    console.log(`${colors.red}✗${colors.reset}  ${email}\n\t${err}`);
+    failed.push(to);
+    console.log(`${colors.red}✗${colors.reset}  ${to}\n\t${err}`);
   }
+  await snooze(500)
 }
 
 async function doWork() {
   try {
+    console.log(`Sending ${emails.length} emails...`);
+    
+    const start = new Date().getTime();
+    
     await asyncPool(2, emails, sendEmail);
+
+    const end = new Date().getTime();
+    
+    fs.writeFileSync('success.json', JSON.stringify(success, null, 2));
+    fs.writeFileSync('failed.json', JSON.stringify(failed, null, 2));
+
+    const duration = end - start;
+    
+    console.log(`Sent ${success.length} emails in ${prettyMs(duration)}`);
+    
+    if (failed.length > 0) {
+      console.log(`${colors.red}There were ${colors.reset}${failed.length}${colors.red} emails that could not be sent.${colors.reset}`)
+      console.log('Please see failed.json for a list of email addresses with errors.')
+    }
   } catch (err) {
     console.log(err);
   }
 }
 
-console.log(`Sending ${emails.length} emails...`);
 doWork();
